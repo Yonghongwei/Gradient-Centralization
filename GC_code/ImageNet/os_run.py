2ef712b04e78b487b76fba14d054524eda3b4b27
Yonghongwei <30529442+Yonghongwei@users.noreply.github.com>
2020-04-07 15:24:26 +0800

    Update os_run.py

--- GC_code/ImageNet/os_run.py
+++ GC_code/ImageNet/os_run.py
@@ -1,8 +1,7 @@
-#cifar100 e200 bs128  gs  2,4,8,16
+
 import os,time
 
 
-time.sleep(100)
-#bs 256 r18
-#os.system("#nohup python -W ignore main.py /ssd/data/yonghw/Imagenet/v0/ --model r50bn --alg sgd -b 256 --path r50bn_sgd1_b256_g4 > logout/r50bn_sgd_b256_g4.log &")
+os.system("#nohup python -W ignore main.py /ssd/data/yonghw/Imagenet/v0/ --model r50bn --alg sgd -b 256 --path r50bn_sgd_b256_g4 > logout/r50bn_sgd_b256_g4.log &")
 
+os.system("#nohup python -W ignore main.py /ssd/data/yonghw/Imagenet/v0/ --model r50bn --alg sgdGC -b 256 --path r50bn_sgdGC_b256_g4 > logout/r50bn_sgdGC_b256_g4.log &")
