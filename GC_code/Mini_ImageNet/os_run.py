6539eecf5227dfa9ba04329c56a13c9e5a0e6a1a
Yonghongwei <30529442+Yonghongwei@users.noreply.github.com>
2020-04-07 14:39:02 +0800

    Update os_run.py

--- GC_code/Mini_ImageNet/os_run.py
+++ GC_code/Mini_ImageNet/os_run.py
@@ -2,11 +2,12 @@
 import os,time
 
 #print('runing mini_imagenet.py')
+
+
 os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgd   > logout/r50_b128_sgd.log  ")
 
 os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgdGC   > logout/r50_b128_sgdGC.log  ")
 
 os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50ws  -b 128 --alg sgd   > logout/r50ws_b128_sgd.log  ")
 
-
-os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgd   > logout/r50ws_b128_sgdGC.log  ")
+os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50ws  -b 128 --alg sgdGC   > logout/r50ws_b128_sgdGC.log  ")
