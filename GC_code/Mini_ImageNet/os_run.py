141d134f490719f318e78cd6435382a930e7a7bd
Yonghongwei <30529442+Yonghongwei@users.noreply.github.com>
2020-04-07 14:37:42 +0800

    Update os_run.py

--- GC_code/Mini_ImageNet/os_run.py
+++ GC_code/Mini_ImageNet/os_run.py
@@ -1,7 +1,7 @@
 #cifar100 e200 bs128  gs  2,4,8,16
 import os,time
 
-#print('runing v11.py')
+#print('runing mini_imagenet.py')
 os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgd   > logout/r50_b128_sgd.log  ")
 
 os.system("nohup  python -W ignore main.py /home/yonghw/data/mini_imagenet/split_mini/ --model r50  -b 128 --alg sgdGC   > logout/r50_b128_sgdGC.log  ")
