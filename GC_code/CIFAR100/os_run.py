1f8c4cb4d158037029fa9a0516959d1080db7649
Yonghongwei <30529442+Yonghongwei@users.noreply.github.com>
2020-04-07 15:20:33 +0800

    Update os_run.py

--- GC_code/CIFAR100/os_run.py
+++ GC_code/CIFAR100/os_run.py
@@ -1,8 +1,1 @@
-#cifar100 e200 bs128  gs  2,4,8,16
-import os,time
 
-
-
-
-os.system("nohup  python  main.py --lr 0.1 --wd 0.0005 --alg sgd --epochs 200  --model r50 > logout/r50_lr11_wd45_sgd.log &")
-os.system("nohup  python  main.py --lr 0.1 --wd 0.0005 --alg sgdGC --epochs 200  --model r50 > logout/r50_lr11_wd45_sgdGC.log &")
