b25ea67da828c8f851ef704d25fd62eb2f2a06c8
YONG Hongwei <30529442+Yonghongwei@users.noreply.github.com>
2020-08-14 19:38:27 +0800

    Update resnet_ws.py

--- GC_code/Mini_ImageNet/resnet_ws.py
+++ GC_code/Mini_ImageNet/resnet_ws.py
@@ -55,10 +55,10 @@ class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.GroupNorm(32,planes)
+        self.bn1 = BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.GroupNorm(32,planes)
+        self.bn2 = BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
 
@@ -87,11 +87,11 @@ class Bottleneck(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
         self.conv1 = conv1x1(inplanes, planes)
-        self.bn1 = nn.GroupNorm(32,planes)
+        self.bn1 = BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes, stride)
-        self.bn2 = nn.GroupNorm(32,planes)
+        self.bn2 = BatchNorm2d(planes)
         self.conv3 = conv1x1(planes, planes * self.expansion)
-        self.bn3 = nn.GroupNorm(32,planes * self.expansion)
+        self.bn3 = BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -126,7 +126,7 @@ class ResNet(nn.Module):
         self.inplanes = 64
         self.conv1 = Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        self.bn1 = nn.GroupNorm(32,64)
+        self.bn1 = BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
@@ -141,7 +141,7 @@ class ResNet(nn.Module):
                 #nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m,nn.GroupNorm):
+            elif isinstance(m,nn.BatchNorm2d):
                 #nn.init.constant_(m.weight, 1)
                 #nn.init.constant_(m.bias, 0)
                 m.weight.data.uniform_()
@@ -163,7 +163,7 @@ class ResNet(nn.Module):
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.GroupNorm(32,planes * block.expansion),
+                BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
